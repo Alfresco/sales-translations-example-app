e7938eb2527a9dc928a23362675994a67b560c8d
Will Abson <will.abson@alfresco.com>
2016-08-24 16:13:55 +0100

    Upgrade doclist component with latest from demo-shell

--- app/components/files/files.component.ts
+++ app/components/files/files.component.ts
@@ -15,80 +15,138 @@
  * limitations under the License.
  */
 
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import {
-  DOCUMENT_LIST_DIRECTIVES,
-  DOCUMENT_LIST_PROVIDERS,
-  DocumentActionsService
+    DOCUMENT_LIST_DIRECTIVES,
+    DOCUMENT_LIST_PROVIDERS,
+    DocumentActionsService,
+    DocumentList,
+    ContentActionHandler,
+    DocumentActionModel,
+    FolderActionModel
 } from 'ng2-alfresco-documentlist';
 import {
-  MDL,
-  AlfrescoContentService,
-  CONTEXT_MENU_DIRECTIVES,
-  AlfrescoPipeTranslate
+    MDL,
+    AlfrescoContentService,
+    CONTEXT_MENU_DIRECTIVES,
+    AlfrescoPipeTranslate
 } from 'ng2-alfresco-core';
+import { PaginationComponent } from 'ng2-alfresco-datatable';
 import { ALFRESCO_ULPOAD_COMPONENTS } from 'ng2-alfresco-upload';
 import { VIEWERCOMPONENT } from 'ng2-alfresco-viewer';
+import { FormService } from 'ng2-activiti-form';
 
 declare let __moduleName: string;
 
 @Component({
-  moduleId: __moduleName,
-  selector: 'files-component',
-  templateUrl: './files.component.html',
-  styleUrls: ['./files.component.css'],
-  directives: [
-    DOCUMENT_LIST_DIRECTIVES,
-    MDL,
-    ALFRESCO_ULPOAD_COMPONENTS,
-    VIEWERCOMPONENT,
-    CONTEXT_MENU_DIRECTIVES
-  ],
-  providers: [DOCUMENT_LIST_PROVIDERS],
-  pipes: [AlfrescoPipeTranslate]
+    moduleId: __moduleName,
+    selector: 'files-component',
+    templateUrl: './files.component.html',
+    styleUrls: ['./files.component.css'],
+    directives: [
+        DOCUMENT_LIST_DIRECTIVES,
+        MDL,
+        ALFRESCO_ULPOAD_COMPONENTS,
+        VIEWERCOMPONENT,
+        CONTEXT_MENU_DIRECTIVES,
+        PaginationComponent
+    ],
+    providers: [DOCUMENT_LIST_PROVIDERS, FormService],
+    pipes: [AlfrescoPipeTranslate]
 })
-export class FilesComponent {
-  currentPath: string = '/Sites/swsdp/documentLibrary';
+export class FilesComponent implements OnInit {
+    currentPath: string = '/Sites/sales-docs/documentLibrary';
 
-  urlFile: string;
-  fileName: string;
-  mimeType: string;
-  fileShowed: boolean = false;
+    fileNodeId: any;
+    fileShowed: boolean = false;
+    multipleFileUpload: boolean = false;
+    folderUpload: boolean = false;
+    acceptedFilesTypeShow: boolean = false;
 
-  acceptedFilesType: string = '.jpg,.pdf,.js';
+    acceptedFilesType: string = '.jpg,.pdf,.js';
 
-  constructor(
-    private contentService: AlfrescoContentService,
-    documentActions: DocumentActionsService) {
-    documentActions.setHandler('my-handler', this.myDocumentActionHandler.bind(this));
-  }
+    @ViewChild(DocumentList)
+    documentList: DocumentList;
 
-  myDocumentActionHandler(obj: any) {
-    window.alert('my custom action handler');
-  }
-
-  myCustomAction1(event) {
-    alert('Custom document action for ' + event.value.entry.name);
-  }
-
-  myFolderAction1(event) {
-    alert('Custom folder action for ' + event.value.entry.name);
-  }
-
-  showFile(event) {
-    if (event.value.entry.isFile) {
-      this.fileName = event.value.entry.name;
-      this.mimeType = event.value.entry.content.mimeType;
-      this.urlFile = this.contentService.getContentUrl(event.value);
-      this.fileShowed = true;
-    } else {
-      this.fileShowed = false;
+    constructor(private contentService: AlfrescoContentService,
+                private documentActions: DocumentActionsService,
+                private formService: FormService,
+                private router: Router) {
+        documentActions.setHandler('my-handler', this.myDocumentActionHandler.bind(this));
     }
-  }
 
-  onFolderChanged(event?: any) {
-    if (event) {
-      this.currentPath = event.path;
+    myDocumentActionHandler(obj: any) {
+        window.alert('my custom action handler');
     }
-  }
+
+    myCustomAction1(event) {
+        alert('Custom document action for ' + event.value.entry.name);
+    }
+
+    myFolderAction1(event) {
+        alert('Custom folder action for ' + event.value.entry.name);
+    }
+
+    showFile(event) {
+        if (event.value.entry.isFile) {
+            this.fileNodeId = event.value.entry.id;
+            this.fileShowed = true;
+        } else {
+            this.fileShowed = false;
+        }
+    }
+
+    onFolderChanged(event?: any) {
+        if (event) {
+            this.currentPath = event.path;
+        }
+    }
+
+    toggleMultipleFileUpload() {
+        this.multipleFileUpload = !this.multipleFileUpload;
+        return this.multipleFileUpload;
+    }
+
+    toggleFolder() {
+        this.multipleFileUpload = false;
+        this.folderUpload = !this.folderUpload;
+        return this.folderUpload;
+    }
+
+    toggleAcceptedFilesType() {
+        this.acceptedFilesTypeShow = !this.acceptedFilesTypeShow;
+        return this.acceptedFilesTypeShow;
+    }
+
+    ngOnInit() {
+        this.formService.getProcessDefinitions().subscribe(
+            defs => this.setupBpmActions(defs || []),
+            err => console.log(err)
+        );
+    }
+
+    viewActivitiForm(event?: any) {
+        this.router.navigate(['/activiti/tasks', '1']);
+    }
+
+    private setupBpmActions(actions: any[]) {
+        actions.map(def => {
+            let documentAction = new DocumentActionModel();
+            documentAction.title = 'Activiti: ' + (def.name || 'Unknown process');
+            documentAction.handler = this.getBpmActionHandler(def);
+            this.documentList.actions.push(documentAction);
+
+            let folderAction = new FolderActionModel();
+            folderAction.title = 'Activiti: ' + (def.name || 'Unknown process');
+            folderAction.handler = this.getBpmActionHandler(def);
+            this.documentList.actions.push(folderAction);
+        });
+    }
+
+    private getBpmActionHandler(processDefinition: any): ContentActionHandler {
+        return function (obj: any, target?: any) {
+            window.alert(`Starting BPM process: ${processDefinition.id}`);
+        }.bind(this);
+    }
 }
